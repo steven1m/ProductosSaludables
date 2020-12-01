991610b91c9906337aebb0319a648ccab8a703e0
steven1m <steven-121997@hotmail.com>
2020-12-01 01:24:54 -0500

    DAO factura y pago

--- src/Modelo/FacturaDAO.java
+++ src/Modelo/FacturaDAO.java
@@ -5,7 +5,13 @@
  */
 package Modelo;
 
+import Servicios.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,22 +20,129 @@ import java.util.ArrayList;
 public class FacturaDAO {
     public int crear (Factura factura){
         int resultado = 0;
+        Connection con ;
+        PreparedStatement ps;
+        String sentencia = "INSERT INTO public.factura(id, cliente_id, empleado_id, pago_id, tipo_venta_id, fecha) "
+                + "VALUES (?, ?, ?, ?, ?, ?)";
+        
+        try{
+            con = Conexion.getConnection();
+            ps = con.prepareStatement(sentencia);
+            ps.setInt(1, factura.getId());
+            ps.setString(2, factura.getClienteId());
+            ps.setInt(3, factura.getEmpleadoId());
+            ps.setFloat(4, factura.getPadoId());
+            ps.setInt(5, factura.getTipoVentaId());
+            java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentTime = sdf.format(factura.getFecha());
+            ps.setString(6, currentTime);
+            
+            resultado = ps.executeUpdate();
+            
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Error : " + 
+                    ex.getMessage());
+        }
         return resultado;
     }
     
-    public ArrayList <Factura> leer (int id){
+    public ArrayList <Factura> leer (String clave, String valor){
         ArrayList <Factura>  lista = new ArrayList<>();
+        Connection con ;
+        PreparedStatement ps ;
+        ResultSet rs ;
         
+        try{
+            con = Conexion.getConnection();
+            String sentencia;
+            
+            if ("".equals(clave) || "".equals(valor)){
+                sentencia = "SELECT * FROM factura;";
+            }else if("nombre".equals(clave)) {
+                sentencia = "SELECT * FROM factura WHERE nombre =?;";
+                
+            }else{
+                sentencia = "SELECT * FROM factura where id=?";
+            }
+            
+           
+            ps = con.prepareStatement(sentencia);
+            
+            if (!"".equals(clave) && !"".equals(valor) ){
+                ps.setString(1, valor);
+            }
+            rs = ps.executeQuery();
+                        
+            Factura factura;
+            while(rs.next()){
+                factura = new Factura();
+                factura.setId(rs.getInt("id"));
+                factura.setClienteId(rs.getString("cliente_id"));
+                factura.setEmpleadoId(rs.getInt("empleado_id"));
+                factura.setPadoId(rs.getInt("pago_id"));
+                factura.setTipoVentaId(rs.getInt("tipo_venta_id"));
+                factura.setFecha(rs.getDate("fecha"));
+                lista.add(factura);
+                
+            }
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Código : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+        }
         return lista;
     }
     
     public int actualizar (Factura factura){
         int resultado = 0;
+        Connection con ;
+        PreparedStatement ps ;
+        String sentencia = "UPDATE public.factura SET "
+                + "id=?, cliente_id=?, empleado_id=?, pago_id=?, tipo_venta_id=?, fecha=? "
+                + "WHERE id=?";
+        
+        try{
+            con = Conexion.getConnection();
+            ps = con.prepareStatement(sentencia);
+            
+            ps.setInt(1, factura.getId());
+            ps.setString(2, factura.getClienteId());
+            ps.setInt(3, factura.getEmpleadoId());
+            ps.setInt(4, factura.getPadoId());
+            ps.setFloat(5, factura.getTipoVentaId());
+            
+            java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentTime = sdf.format(factura.getFecha());
+            
+            ps.setString(6, currentTime);
+            ps.setInt(7, factura.getId());
+            
+            resultado = ps.executeUpdate();
+            
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Error : " + 
+                    ex.getMessage());
+        }
         return resultado;
     }
     
     public int borrar (int id){
         int resultado = 0;
+        Connection con ;
+        PreparedStatement ps ;
+        String sentencia = "DELETE FROM public.factura "
+                + "WHERE id=?";
+        
+        try{
+            con = Conexion.getConnection();
+            ps = con.prepareStatement(sentencia);
+            ps.setInt(1, id);
+            
+            resultado = ps.executeUpdate();
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Error : " + 
+                    ex.getMessage());
+        }
         return resultado;
     }
 }
--- src/Modelo/PagoDAO.java
+++ src/Modelo/PagoDAO.java
@@ -8,6 +8,7 @@ package Modelo;
 import Servicios.Conexion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
@@ -46,19 +47,103 @@ public class PagoDAO {
         return resultado;
     }
     
-    public ArrayList <Pago> leer (int id){
+    public ArrayList <Pago> leer (String clave, String valor){
         ArrayList <Pago>  lista = new ArrayList<>();
+        Connection con ;
+        PreparedStatement ps ;
+        ResultSet rs ;
         
+        try{
+            con = Conexion.getConnection();
+            String sentencia;
+            
+            if ("".equals(clave) || "".equals(valor)){
+                sentencia = "SELECT * FROM pago;";
+            }else if("nombre".equals(clave)) {
+                sentencia = "SELECT * FROM pago WHERE nombre =?;";
+                
+            }else{
+                sentencia = "SELECT * FROM pago where id=?";
+            }
+            
+           
+            ps = con.prepareStatement(sentencia);
+            
+            if (!"".equals(clave) && !"".equals(valor) ){
+                ps.setString(1, valor);
+            }
+            rs = ps.executeQuery();
+                        
+            Pago pago;
+            while(rs.next()){
+                pago = new Pago();
+                pago.setId(rs.getInt("id"));
+                pago.setValor(rs.getFloat("valor"));
+                
+                pago.setFecha(rs.getDate("fecha"));
+                
+                pago.setTipoPagoId(rs.getInt("tipo_pago_id"));
+                pago.setEmpleadoId(rs.getInt("empleado_id"));
+                lista.add(pago);
+                
+            }
+        }
+        catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Código : " + 
+                        ex.getErrorCode() + "\nError :" + ex.getMessage());
+        }
         return lista;
     }
     
     public int actualizar (Pago pago){
         int resultado = 0;
+        Connection con ;
+        PreparedStatement ps ;
+        String sentencia = "UPDATE public.pago SET  "
+                + "id=?, valor=?, fecha=?, tipo_pago_id=?, empleado_id=? "
+                + "WHERE id=?";
+        
+        try{
+            con = Conexion.getConnection();
+            ps = con.prepareStatement(sentencia);
+            
+            ps.setInt(1, pago.getId());
+            ps.setFloat(2, pago.getValor());
+            
+            java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentTime = sdf.format(pago.getFecha());
+            ps.setString(3, currentTime);
+            
+            ps.setInt(4, pago.getTipoPagoId());
+            ps.setFloat(5, pago.getEmpleadoId());
+            ps.setInt(6, pago.getId());
+            resultado = ps.executeUpdate();
+            
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Error : " + 
+                    ex.getMessage());
+        }
+        
         return resultado;
     }
     
     public int borrar (int id){
         int resultado = 0;
+        Connection con ;
+        PreparedStatement ps ;
+        String sentencia = "DELETE FROM public.pago "
+                + "WHERE id=?";
+        
+        try{
+            con = Conexion.getConnection();
+            ps = con.prepareStatement(sentencia);
+            ps.setInt(1, id);
+            
+            resultado = ps.executeUpdate();
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Error : " + 
+                    ex.getMessage());
+        }
         return resultado;
     }
 }
